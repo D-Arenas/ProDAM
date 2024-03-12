0f65be44f2a217338abf1e6e131d989d3b00b52b
Sebastian Urrea <sebastian.urrea@caseware.com>
2024-03-12 13:05:59 -0500

    CI/CD Test Number 2

--- DevOps/setup.sh
+++ DevOps/setup.sh
@@ -5,6 +5,7 @@
 sudo yum update -y
 sudo yum install python3-pip -y
 sudo yum install git -y
+sudo yum install screen -y
 
 pip install virtualenv
 virtualenv myenv
@@ -14,4 +15,4 @@ git clone https://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@github.com/D-Arenas/P
 cd Project-1-analitica/despliegue
 
 pip install -r requirements.txt
-python app.py
+screen -S dashboard_session -d -m python app.py
